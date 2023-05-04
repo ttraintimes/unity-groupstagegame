8c6b32ce585950ebe5b9459d5f856e30a7f4191b
ttraintimes <ttraintimes@gmail.com>
2023-05-04 14:11:01 +0100

    update

---
+++ unity project/Assets/scripts/PlayVideoOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideoOnCollision : MonoBehaviour
+{
+    public GameObject videoPlayerObject;
+
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = videoPlayerObject.GetComponent<VideoPlayer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            videoPlayer.Play();
+        }
+    }
+}
