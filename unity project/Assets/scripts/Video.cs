44d67857a096da15a447b3423ccdedafef68513a
iperryxd <dtnvrry@ucl.ac.uk>
2023-05-05 16:53:29 +0100

    Stage3 video popup

---
+++ unity project/Assets/scripts/Video.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour
+{
+    public GameObject videoPlayer;
+    public int timeToStop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider player)
+    {
+        videoPlayer.SetActive(true);
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        videoPlayer.SetActive(false);
+    }
+
+}
