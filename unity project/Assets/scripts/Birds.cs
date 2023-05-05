fe44239d0fac9dc29a6aceeea37b234db0cc7767
iperryxd <dtnvrry@ucl.ac.uk>
2023-05-05 14:16:39 +0100

    Further additions to Stage1, notes for all stages

---
+++ unity project/Assets/scripts/Birds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birds : MonoBehaviour
+{
+    public AudioSource playSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        playSound.Stop();
+        
+    }
+}
